334d78df48241a196904eef3e975afe5ee1ad2cf
Aby varghese <56151184+AbyvargheseMandapathel@users.noreply.github.com>
2022-09-18 08:40:45 +0000

    udp Added

---
+++ .ccls-cache/@home@runner@labs6cn/uclient.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<string.h>
+
+void main (int argc ,char **argv){
+  if(argc !=2){
+    printf("error ");
+    exit(0);
+  }
+  int port = atoi(argv[1]);
+  int sockfd;
+  struct sockaddr_in serveraddr;
+  char buffer[1024];
+  socklen_t addr_size;
+
+  sockfd=socket(AF_INET,SOCK_DGRAM,0);
+  memset(&serveraddr,'\0',sizeof(serveraddr));
+
+  serveraddr.sin_family=AF_INET;
+  serveraddr.sin_port = htons(port);
+  serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  strcpy(buffer,"hello");
+  sendto(sockfd,buffer,1024,0,(struct sockaddr*)&serveraddr,sizeof(serveraddr);
+
+  printf("data send:%s",buffer);
+  
+}
